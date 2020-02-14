3b12485fcf632e1dba74c30f9dab24a3480409da
goce-arsoski <goce_ars@yahoo.com>
2020-02-14 20:41:54 +0100

    first git exercise

--- ruby_ex6.rb
+++ ruby_ex6.rb
@@ -5,8 +5,8 @@ y = "Those who know #{binary} and those who #{do_not}."
 
 puts x 
 puts y
-puts "I said: #{x}."
-puts "I also said: '#{y}'."
+puts "I said: #{x}"
+puts "I also said: '#{y}'"
 
 hilarious = false
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
