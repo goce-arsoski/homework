9d391ef88c5ed9b0b4710b73655f4aebd6a48718
goce-arsoski <goce_ars@yahoo.com>
2020-02-14 20:46:28 +0100

    first git exercise

--- ruby_ex6.rb
+++ ruby_ex6.rb
@@ -6,12 +6,12 @@ y = "Those who know #{binary} and those who #{do_not}."
 puts x 
 puts y
 puts "I said: #{x}"
-puts "I also said: '#{y}'"
+print "I also said: '#{y}'"
 
 hilarious = false
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 puts joke_evaluation
 
-w = "This is the left side of ..."
+w = "This is the left side of..."
 e = "a string with a right side."
 puts w + e
