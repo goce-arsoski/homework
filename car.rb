45bad3490b279206e767d12596b3987a59f2eb14
goce-arsoski <goce_ars@yahoo.com>
2020-02-26 23:56:29 +0100

    car.rb_modified

--- car.rb
+++ car.rb
@@ -1,4 +1,6 @@
 class MyCar
+    attr_accessor :color
+    attr_reader :year
     def initialize(year, color, brand, model)
         @year = year
         @color = color
@@ -7,43 +9,18 @@ class MyCar
         @speed = 0
         puts "Please insert following data for your car: year of production, color, brand and model."
         puts "Insert '1' for acceleration, '2' for deceleration and '0' to shut off the car."
+        puts "If you wanna see the color list, please enter 'color' or number from 0 to 12 if you know the right color number."
     end
-    
-    def year(year)
-        @year = year
-    end
-    
-    def color(color)
-        @color = color
-    end
-    
-    def brand(brand)
-        @brand = brand
-    end
-    
-    def model(model)
-        @model = model
-    end
-    
+
     def car_info
         puts "You are driving car produced #{@year} year with #{@color} color, brand #{@brand}, model #{@model}."
     end
     
     def drive(state)
-        if state == 1
-            return acceleration
-        end
-
-        if state ==2
-            return deceleration
-        end
-
-        if state == 0
-            return shut_off
-        end
+        return acceleration if state == 1
+        return deceleration if state == 2
+        return shut_off if state.zero?
     end
-    
-    private
     
     def acceleration
         if @speed < 200
@@ -62,17 +39,35 @@ class MyCar
     def shut_off
         if @speed.zero?
             @speed = 0 
-            puts "You shuted off the car!"
+            puts "You shutted off the car!"
         else
             puts "You can't shut off the car when car is stil moving!"
         end
     end
+
+    def spray_paint(color)
+        array_color = ['Alpine White', 'Arctic Metallic', 'Barrique Red', 'Black', 'Black Sapphire Metallic', 'Crimson Red', 'Deep Green', 'LeMans Blue', 'Monaco Blue', 'Platinum Bronze', 'Space Gray', 'Sparkling Graphite', 'Titanium Silver']
+        if color == 'color'
+            array_color.each_with_index do |num, idx|
+            puts "For #{num} insert -#{idx}-"
+            end
+        else
+            array_color.each_with_index do |num, idx|
+            if color == idx
+                color = num
+            end
+            end
+            if @color != color
+                puts "You have changed your car color from #{@color} to #{color}."
+                @color = color
+            else
+                puts "You choose same color."
+            end
+        end
+    end
 end
-    
-    mycar = MyCar.new(2007, "silver", "BMW", "325i")
+
+    mycar = MyCar.new(2007, 'Titanium Silver', 'BMW', '325i')
     mycar.car_info
-    
-    mycar.drive(1)
-    mycar.drive(0)
-    mycar.drive(2)
-    mycar.drive(0)
+    mycar.spray_paint(7)
+    mycar.car_info
